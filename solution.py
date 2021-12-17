77153dcd1c2c152d613bd2124f4eaadc0b21b9d0
dannixx <asd>
2021-12-17 19:33:22 +0200

    first attempt

---
+++ solution.py
@@ -0,0 +1,221 @@
+"""Stereo matching."""
+import numpy as np
+from scipy.signal import convolve2d
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def ssd_distance(left_image: np.ndarray,
+                     right_image: np.ndarray,
+                     win_size: int,
+                     dsp_range: int) -> np.ndarray:
+        """Compute the SSDD distances tensor.
+
+        Args:
+            left_image: Left image of shape: HxWx3, and type np.double64.
+            right_image: Right image of shape: HxWx3, and type np.double64.
+            win_size: Window size odd integer.
+            dsp_range: Half of the disparity range. The actual range is
+            -dsp_range, -dsp_range + 1, ..., 0, 1, ..., dsp_range.
+
+        Returns:
+            A tensor of the sum of squared differences for every pixel in a
+            window of size win_size X win_size, for the 2*dsp_range + 1
+            possible disparity values. The tensor shape should be:
+            HxWx(2*dsp_range+1).
+        """
+        num_of_rows, num_of_cols = left_image.shape[0], left_image.shape[1]
+        disparity_values = range(-dsp_range, dsp_range+1)
+        ssdd_tensor = np.zeros((num_of_rows,
+                                num_of_cols,
+                                len(disparity_values)))
+        """INSERT YOUR CODE HERE"""
+
+        as_strided = np.lib.stride_tricks.as_strided
+
+        def pad_image_one_side(img, amount, side):
+            if side == 'l':
+                img = np.stack(
+                    [np.pad(img[:, :, c], ((0, 0), (amount, 0)), mode='constant', constant_values=0) for c in range(img.shape[-1])],
+                    axis=2)
+            else:
+                img = np.stack(
+                    [np.pad(img[:, :, c], ((0, 0), (0, amount)), mode='constant', constant_values=0) for c in range(img.shape[-1])],
+                    axis=2)
+            return img
+
+        def pad_image(img, pad):
+            img = np.pad(img, pad, mode='constant', constant_values=0)
+            return img
+
+        def disperity_shift_diff(left_image, right_image, dsp):
+            if dsp > 0:
+                left_image_shifted = pad_image_one_side(left_image, dsp, 'r')
+                right_image_shifted = pad_image_one_side(right_image, dsp, 'l')
+            elif dsp < 0:
+                left_image_shifted = pad_image_one_side(left_image, -1 * dsp, 'l')
+                right_image_shifted = pad_image_one_side(right_image, -1 * dsp, 'r')
+            else:
+                left_image_shifted, right_image_shifted = left_image, right_image
+            diff = (left_image_shifted - right_image_shifted) ** 2
+            diff = diff.sum(axis=-1)
+            if dsp > 0:
+                return diff[:, :diff.shape[1] - dsp]
+            else:
+                return diff[:, -1 * dsp:]
+
+        def strided_view(A, kernel_size, stride):
+            output_shape = ((A.shape[0] - kernel_size) // stride + 1,
+                            (A.shape[1] - kernel_size) // stride + 1)
+
+            shape_w = (output_shape[0], output_shape[1], kernel_size, kernel_size)
+            strides_w = (stride * A.strides[0], stride * A.strides[1], A.strides[0], A.strides[1])
+
+            A_w = as_strided(A, shape_w, strides_w)
+            return A_w
+
+        def calc_disperity(left_image, right_image, win_size, dsp):
+            padding_size = int(np.floor(win_size / 2))
+            diff = disperity_shift_diff(left_image, right_image, dsp)
+            padded_diff = pad_image(diff, padding_size)
+            sum_pool = strided_view(padded_diff, win_size, 1).sum(axis=(2, 3))
+            return sum_pool
+
+        ssdd_tensor = np.stack([calc_disperity(left_image, right_image, win_size, d) for d in disparity_values], axis=-1)
+
+        ssdd_tensor -= ssdd_tensor.min()
+        ssdd_tensor /= ssdd_tensor.max()
+        ssdd_tensor *= 255.0
+        return ssdd_tensor
+
+    @staticmethod
+    def naive_labeling(ssdd_tensor: np.ndarray) -> np.ndarray:
+        """Estimate a naive depth estimation from the SSDD tensor.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for every
+            pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+
+        Evaluate the labels in a naive approach. Each value in the
+        result tensor should contain the disparity matching minimal ssd (sum of
+        squared difference).
+
+        Returns:
+            Naive labels HxW matrix.
+        """
+        # you can erase the label_no_smooth initialization.
+        label_no_smooth = np.zeros((ssdd_tensor.shape[0], ssdd_tensor.shape[1]))
+        """INSERT YOUR CODE HERE"""
+        return label_no_smooth
+
+    @staticmethod
+    def dp_grade_slice(c_slice: np.ndarray, p1: float, p2: float) -> np.ndarray:
+        """Calculate the scores matrix for slice c_slice.
+
+        Calculate the scores slice which for each column and disparity value
+        states the score of the best route. The scores slice is of shape:
+        (2*dsp_range + 1)xW.
+
+        Args:
+            c_slice: A slice of the ssdd tensor.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+        Returns:
+            Scores slice which for each column and disparity value states the
+            score of the best route.
+        """
+        num_labels, num_of_cols = c_slice.shape[0], c_slice.shape[1]
+        l_slice = np.zeros((num_labels, num_of_cols))
+        """INSERT YOUR CODE HERE"""
+        return l_slice
+
+    def dp_labeling(self,
+                    ssdd_tensor: np.ndarray,
+                    p1: float,
+                    p2: float) -> np.ndarray:
+        """Estimate a depth map using Dynamic Programming.
+
+        (1) Call dp_grade_slice on each row slice of the ssdd tensor.
+        (2) Store each slice in a corresponding l tensor (of shape as ssdd).
+        (3) Finally, for each pixel in l (along each row and column), choose
+        the best disparity value. That is the disparity value which
+        corresponds to the lowest l value in that pixel.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for every
+            pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+        Returns:
+            Dynamic Programming depth estimation matrix of shape HxW.
+        """
+        l = np.zeros_like(ssdd_tensor)
+        """INSERT YOUR CODE HERE"""
+        return self.naive_labeling(l)
+
+    def dp_labeling_per_direction(self,
+                                  ssdd_tensor: np.ndarray,
+                                  p1: float,
+                                  p2: float) -> dict:
+        """Return a dictionary of directions to a Dynamic Programming
+        etimation of depth.
+
+        For each direction in 1, ..., 8, calculate scores tensors
+        according to dp_grade_slice and the method which allows you to
+        extract slices along each direction.
+
+        You may use helper methods (functions) that you write on your own.
+        We found `np.diagonal` to be very helpful to extract diagonal slices.
+        `np.unravel_index` might be helpful if you're thinking in MATLAB
+        notations: it's the ind2sub equivalent.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for
+            every pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+
+        Returns:
+            Dictionary int->np.ndarray which maps each direction to the
+            corresponding dynamic programming estimation of depth based on
+            that direction.
+        """
+        num_of_directions = 8
+        l = np.zeros_like(ssdd_tensor)
+        direction_to_slice = {}
+        """INSERT YOUR CODE HERE"""
+        return direction_to_slice
+
+    def sgm_labeling(self, ssdd_tensor: np.ndarray, p1: float, p2: float):
+        """Estimate the depth map according to the SGM algorithm.
+
+        For each direction in 1, ..., 8, calculate scores tensors
+        according to dp_grade_slice and the method which allows you to
+        extract slices along each direction.
+
+        You may use helper methods (functions) that you write on your own.
+        We found `np.diagonal` to be very helpful to extract diagonal slices.
+        `np.unravel_index` might be helpful if you're thinking in MATLAB
+        notations: it's the ind2sub equivalent.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for
+            every pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+
+        Returns:
+            Semi-Global Mapping depth estimation matrix of shape HxW.
+        """
+        num_of_directions = 8
+        l = np.zeros_like(ssdd_tensor)
+        """INSERT YOUR CODE HERE"""
+        return self.naive_labeling(l)
+
